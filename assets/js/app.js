717ff265b4e945f5da01c4354a3ca122c678555f
Dani Carter <carte470@d.umn.edu>
2018-03-23 22:52:15 -0500

    mixitup script work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,27 @@
 var mixer = mixitup(".container", {
-	selectors: {
-		target: '.mix'
-	}, 
-	animation: {
-		duration: 300
-	}
+	// selectors: {
+	// 	target: '.mix'
+	// }, 
+	// animation: {
+	// 	duration: 300
+	// }
 });
 
 $("#all-button").on ("click", function() {
-	mixer.filter('all')
+	mixer.filter('.all');
 });
 
 $("#node-button").on ("click", function() {
-	console.log("Node button selected.")
-	mixer.filter('node')
-})
+	console.log("Node button selected.");
+	mixer.filter('.node');
+});
 
 $("#react-button").on ("click", function() {
-	console.log("React button selected.")
-	mixer.filter('react')
-})
+	console.log("React button selected.");
+	mixer.filter('.react');
+});
 
 $("#js-button").on ("click", function() {
-	console.log("Javascript button selected.")
-	mixer.filter('js')
-})
+	console.log("Javascript button selected.");
+	mixer.filter('.js');
+});
