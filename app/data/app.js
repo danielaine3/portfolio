fb4e1e790172c0ad17ab098bdaa2b6271b8c1936
Dani Carter <carte470@d.umn.edu>
2018-03-26 17:28:33 -0500

    added routes

---
+++ app/data/app.js
@@ -0,0 +1,29 @@
+var mixitup = require('mixitup');
+var container = document.querySelector('#gallery');
+var config = {
+	"animation": {
+        "duration": 607,
+        "nudge": true,
+        "reverseOut": false,
+        "effects": "fade scale(0.01) stagger(70ms)"
+	}
+};
+
+var mixer = mixitup(container, config);
+
+// console.log(mixer);
+
+// container.classList.add('mixitup-ready');
+// mixer.show()
+	// .then(function() {
+	// 	mixer.configure({
+	// 		animation: {
+	// 			effects: 'fade scale',
+	// 			duration:300
+	// 		}
+	// 	});
+	// });
+
+// $(function() {
+// 	$('#gallery').mixitup(container, config);
+// });
