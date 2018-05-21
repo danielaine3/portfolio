3273870334cc0d676ac1a14fc002ce48be4514bf
Dani Carter <carte470@d.umn.edu>
2018-05-20 21:58:59 -0500

    start of hover on gallery

--- app/public/app.js
+++ app/public/app.js
@@ -24,7 +24,57 @@ $(window).scroll(function(){
     $(".sidebar").css("top", Math.max(25, 750 - $(this).scrollTop()));
 });
 
+// var modalText = {
+//     intlintern: {
+//         title: 'International Intern',
+//         description: 
+//         tag:
+//         link:
+//     },
+//     intlintern: {
+//         title: 'International Intern',
+//         description: 
+//         tag:
+//         link:
+//     },
+//     intlintern: {
+//         title: 'International Intern',
+//         description: 
+//         tag:
+//         link:
+//     },
+//     intlintern: {
+//         title: 'International Intern',
+//         description: 
+//         tag:
+//         link:
+//     },
+//     intlintern: {
+//         title: 'International Intern',
+//         description: 
+//         tag:
+//         link:
+//     },
+//     intlintern: {
+//         title: 'International Intern',
+//         description: 
+//         tag:
+//         link:
+//     },
+// };
 
+// $('')
+
+
+
+// function showModal(id) {
+//     $('#modal .modalTitle').text(modalText[id].title);
+//     $('#modal .modaldesc').text(modalText[id].description);
+//     $('#modal .tag').text(modalText[id].tag);
+//     if (modalText[id].link) $('#modal .button').addClass('visible')
+//         .parent()
+//         .attr('href', modalText[id].link)
+// }
 // $(".gallerypic").hover(function() {
-    
+//     $(".modal")
 // })
